faabd2d5e2c2d9dffbe46e1ce7081fa63bb12c71
StaceyO21 <staceyobrien21@gmail.com>
2024-12-12 21:39:16 -0600

    spa day signup

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,35 @@
+package org.launchcode.controllers;
+
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Objects;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+
+    @GetMapping("")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+    @PostMapping("")
+    public String createUser(Model model, @ModelAttribute User user,String verify) {
+
+        if(!user.getPassword().equals(verify)){
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
+        model.addAttribute("user", user);
+        return "user/index";
+    }
+
+}
---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,57 @@
+package org.launchcode.models;
+
+import java.util.*;
+
+public class User {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    private static final List<User> users = new ArrayList<>();
+
+//
+//    public static void add(User newUser) {
+//        User.add(newUser);
+//    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
